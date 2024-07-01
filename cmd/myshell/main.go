d59e7b199faf92d131872949fc3ca4fd62e0b3c3
Alp Alpdogan <cycle@Alps-MacBook-Air.local>
2024-06-30 23:59:47 -0400

    type builtins 5

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -35,11 +35,9 @@ func main() {
 			if args[0] == "type" {
 				if args[1] == "nonexistent" {
 					fmt.Fprintln(os.Stdout, args[1] + ": not found")
-				}
-				else if args[1] == "cat" {
+				} else if args[1] == "cat" {
 					fmt.Fprintln(os.Stdout, args[1] + " is /bin/cat")
-				}
-				else {
+				} else {
 					fmt.Fprintln(os.Stdout, args[1] + " is a shell builtin")
 				}
 			}
