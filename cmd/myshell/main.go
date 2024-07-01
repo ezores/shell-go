102fad2e8a9b5a5b8378c2a6e4d809fc3660d444
Alp Alpdogan <cycle@Alps-MacBook-Air.local>
2024-06-30 23:24:37 -0400

    echo builtin 3

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -1,39 +1,36 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
+    "bufio"
+    "fmt"
+    "os"
+    "strings"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	//fmt.Println("Logs from your program will appear here!")
-	for{
-		// Uncomment this block to pass the first stage
-		fmt.Fprint(os.Stdout, "$ ")
+    for {
+        // Print prompt
+        fmt.Fprint(os.Stdout, "$ ")
 
-		// Wait for user input
-		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
-		
-		if(err != nil) {
-			panic(err)
-		}
-		
-		cmd := strings.TrimSpace(input)
+        // Wait for user input
+        input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+        
+        if err != nil {
+            panic(err)
+        }
+        
+        cmd := strings.TrimSpace(input)
 
-		switch cmd {
-		case "exit 0": 
-			os.Exit(0)
-		case "echo"
-				args := strings.Split(cmd, " ")
-				if args[0] == "echo" {
-					fmt.Fprintln(os.Stdout, strings.Join(args[1:], " "))
-				}
-		default:
-			fmt.Fprint(os.Stdout, cmd +": command not found\n")		
-	}
-	
-}
+        switch {
+        case cmd == "exit 0": 
+            os.Exit(0)
+        case strings.HasPrefix(cmd, "echo"):
+            args := strings.Split(cmd, " ")
+            if args[0] == "echo" {
+                fmt.Fprintln(os.Stdout, strings.Join(args[1:], " "))
+            }
+        default:
+            fmt.Fprint(os.Stdout, cmd +": command not found\n")		
+        }
+    }
 }
